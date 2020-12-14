72a16265e7961cee036c4569bc1fab9a9a9e4d74
frankrenan <frank.renan.t.lima@gmail.com>
2020-12-14 19:26:49 -0400

    Enviando dados relativos ao pagamento

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const consign = require('consign');
+const bodyparser = require('body-parser'); // traduzir as requisições do POST
 
 
 module.exports = () => {
     const app = express();
+
+    app.use(bodyparser.urlencoded({extended: true}));
+    app.use(bodyparser.json());
 
     consign()
         .include('controllers')

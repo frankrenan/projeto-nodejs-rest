8736cbabf06ccf8c42b564a072821f5eec0aa3d9
frank.lima <frank.lima@tce.am.gov.br>
2020-12-15 14:28:01 -0400

    validação de entrada

--- model/antedimento.js
+++ model/antedimento.js
@@ -3,20 +3,47 @@ const conexao = require('../database/conexao')
 
 class Atendimento {
 
-    adiciona(atendimento, res){
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH/MM/SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO atendimentos SET ?';
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados )=> {
-            if (erro){
-                res.status(400).json(erro);
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length > 4;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
-            else{
-                res.status(201).json(resultados);
-            }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        }
+        else {
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+
+
+            const sql = 'INSERT INTO atendimentos SET ?';
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro);
+                }
+                else {
+                    res.status(201).json(resultados);
+                }
+            })
+        }
 
     }
 

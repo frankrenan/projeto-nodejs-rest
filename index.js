97e51555808a8eecbdbc8823cce65af9e23c5a2f
frank.lima <frank.lima@tce.am.gov.br>
2020-12-15 10:32:19 -0400

    criando as tabelas

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./database/conexao');
+const Tables = require ('./database/tables');
 
 conexao.connect(error => {
     if (error) {
         console.log(error)
     } else {
         console.log('rodando com sucesso');
+
+        Tables.init(conexao);
 
         const app = customExpress();
 

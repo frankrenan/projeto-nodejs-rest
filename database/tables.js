e185e2b61a70f3fe69a2caca090f6db08ab2193a
frank.lima <frank.lima@tce.am.gov.br>
2020-12-15 12:10:42 -0400

    Adicionando data no atendimento com biblioteca Moment

--- database/tables.js
+++ database/tables.js
@@ -1,18 +1,18 @@
 class Tables {
 
-    init(conexao){
+    init(conexao) {
         this.conexao = conexao;
         this.criarAtendimentos();
     }
 
-    criarAtendimentos(){
-        const sql = 'CREATE TABLE IF NOT EXISTS atendimentos (id int NOT NULL auto_increment, cliente varchar(50) NOT NULL, pet varchar(50), servico varchar(50) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))'
+    criarAtendimentos() {
+        const sql = 'CREATE TABLE IF NOT EXISTS atendimentos (id int NOT NULL auto_increment, cliente varchar(50) NOT NULL, pet varchar(50), servico varchar(50) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))'
 
         this.conexao.query(sql, error => {
-            if(error){
+            if (error) {
                 console.log(error);
             }
-            else{
+            else {
                 console.log('Tabelas atendimentos criadas com sucesso!');
             }
         });
